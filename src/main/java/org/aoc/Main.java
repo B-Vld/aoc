67900bcb445a844a8bcfff7f037d66f81a62061b
B-Vld <vlad.beiusanu@gmail.com>
2022-12-08 21:28:37 +0200

    minor adjustments

--- src/main/java/org/aoc/Main.java
+++ src/main/java/org/aoc/Main.java
@@ -1,45 +1,24 @@
 package org.aoc;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aoc.utils.ChallengeFetcher;
 
-import java.lang.reflect.InvocationTargetException;
 import java.time.LocalDate;
-
-import static java.lang.String.format;
 
 @Slf4j
 public class Main {
 
-    private static final int DAY_OF_MONTH = LocalDate.now().getDayOfMonth();
-    private static final String CHALLENGES = "org.aoc.challenges.";
-
     public static void main(String[] args) {
+        final var DAY_OF_MONTH = LocalDate.now().getDayOfMonth();
         if (DAY_OF_MONTH <= 25) {
-            Challenge challenge = getChallenge();
-            assert challenge != null;
-            challenge.firstChallenge(getFileName());
-            challenge.secondChallenge(getFileName());
+            var challengePair = ChallengeFetcher.challengeInputPair(DAY_OF_MONTH);
+            var challenge = challengePair.getLeft();
+            var inputFile = challengePair.getRight();
+            challenge.firstChallenge(inputFile);
+            challenge.secondChallenge(inputFile);
         } else {
             log.info("AoC22 finished");
         }
-    }
-
-    static String getClassName() {
-        return format("Day%d", Main.DAY_OF_MONTH);
-    }
-
-    static Challenge getChallenge() {
-        try {
-            return (Challenge) Class.forName(format("%s%s", CHALLENGES, getClassName())).getDeclaredConstructor().newInstance();
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException |
-                 ClassNotFoundException e) {
-            log.error(format("Challenge not found for day %d", DAY_OF_MONTH));
-        }
-        return null;
-    }
-
-    static String getFileName() {
-        return format("day%d.txt", DAY_OF_MONTH);
     }
 
 }
---
+++ src/main/java/org/aoc/utils/ChallengeFetcher.java
@@ -0,0 +1,35 @@
+package org.aoc.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aoc.Challenge;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.lang.reflect.InvocationTargetException;
+
+import static java.lang.String.format;
+
+@Slf4j
+public class ChallengeFetcher {
+
+    private static final String CHALLENGES_DAY = "org.aoc.challenges.Day";
+
+    public static Pair<Challenge, String> challengeInputPair(int day) {
+        return Pair.of(getChallenge(day), getInput(day));
+    }
+
+    private static Challenge getChallenge(int day) {
+        try {
+            return (Challenge) Class.forName(format("%s%d", CHALLENGES_DAY, day)).getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException |
+                 ClassNotFoundException e) {
+            log.error(format("Challenge not found for day %d", day));
+        }
+        return null;
+    }
+
+    private static String getInput(int day) {
+        return format("day%d.txt", day);
+    }
+
+
+}
